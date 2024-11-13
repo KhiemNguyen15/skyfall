52a817051342a15d9ce7e307106cac4ff89cc952
KhiemNguyen15 <khiemsa15@gmail.com>
2024-11-13 14:16:19 -0600

    feat: Create User and ShareRequest model classes

---
+++ app/src/main/java/com/github/skyfall/data/model/ShareRequest.java
@@ -0,0 +1,14 @@
+package com.github.skyfall.data.model;
+
+import java.util.Date;
+
+public class ShareRequest {
+    private String senderUid;
+    private String recipientUid;
+    private String fileUri;
+    private String fileType;
+    private String deviceType;
+    private Date timestamp;
+
+    public ShareRequest() {}
+}
---
+++ app/src/main/java/com/github/skyfall/data/model/User.java
@@ -0,0 +1,9 @@
+package com.github.skyfall.data.model;
+
+public class User {
+    private String uid;
+    private String displayName;
+    private String photoURL;
+
+    public User() {}
+}
