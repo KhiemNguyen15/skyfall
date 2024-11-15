91aa21629877c93f43d208ae5874f51fd6e31502
KhiemNguyen15 <khiemsa15@gmail.com>
2024-11-15 09:03:45 -0600

    chore: Create get/set methods for model classes

--- app/src/main/java/com/github/skyfall/data/model/User.java
+++ app/src/main/java/com/github/skyfall/data/model/User.java
@@ -6,4 +6,28 @@ public class User {
     private String photoURL;
 
     public User() {}
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
 }
