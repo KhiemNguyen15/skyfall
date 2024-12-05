d6d14178ee2e8a59042fe36c8c222e6bbf947e0e
Zachary Ohlenforst <zohlenforst@gmail.com>
2024-12-05 16:42:52 -0600

    feat: Add logout button and functionality to SettingsActivity

--- app/src/main/java/com/github/skyfall/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/github/skyfall/ui/settings/SettingsActivity.java
@@ -2,16 +2,23 @@ package com.github.skyfall.ui.settings;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.skyfall.R;
+import com.github.skyfall.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button logoutButton = findViewById(R.id.logoutButton);
 
         if (findViewById(R.id.idFrameLayout) != null) {
             if (savedInstanceState != null) {
@@ -22,6 +29,20 @@ public class SettingsActivity extends AppCompatActivity {
                     .replace(R.id.idFrameLayout, new SettingsFragment())
                     .commit();
         }
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Could not logout", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public Intent launchPasswordActivity() {
