5b4d33770844b223f4512f18c115657cb8e2075b
zachattack168 <145884553+zachattack168@users.noreply.github.com>
2024-12-05 23:50:03 -0600

    Issue-42 Add logout button and functionality to SettingsActivity (#45)
    * feat: Add logout button and functionality to SettingsActivity
    
    * refactor: Organize and reformat code to merge with master
    
    * chore: Change log out button to start FirebaseAuthActivity
    
    * fix: Clear the back stack to prevent backtracking after logout
    
    ---------
    
    Co-authored-by: KhiemNguyen15 <khiemsa15@gmail.com>
    Co-authored-by: Justin Lopez <justinlopez22003@gmail.com>

--- app/src/main/java/com/github/skyfall/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/github/skyfall/ui/settings/SettingsActivity.java
@@ -2,16 +2,22 @@ package com.github.skyfall.ui.settings;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.skyfall.R;
+import com.github.skyfall.ui.login.FirebaseAuthActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button logoutButton = findViewById(R.id.logoutButton);
 
         if (findViewById(R.id.idFrameLayout) != null) {
             if (savedInstanceState != null) {
@@ -22,6 +28,20 @@ public class SettingsActivity extends AppCompatActivity {
                     .replace(R.id.idFrameLayout, new SettingsFragment())
                     .commit();
         }
+
+        logoutButton.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_LONG).show();
+                // Clear the back stack and start the login activity
+                Intent intent = new Intent(getApplicationContext(), FirebaseAuthActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "Could not logout", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public Intent launchPasswordActivity() {
