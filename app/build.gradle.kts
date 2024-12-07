7b52d6907e5045b6a38b1cd2de17c9158f9cf684
KhiemNguyen15 <khiemsa15@gmail.com>
2024-12-06 23:26:38 -0600

    chore: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.skyfall"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.1.0-alpha.1"
+        versionCode = 4
+        versionName = "0.2.0-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
