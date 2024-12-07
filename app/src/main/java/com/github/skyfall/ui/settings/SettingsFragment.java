443580329ae389f2b40903859db8db211d424361
zachattack168 <145884553+zachattack168@users.noreply.github.com>
2024-12-06 18:34:10 -0600

    Issue-46 Add delete account button to SettingsActivity (#55)
    * feat: Add functionality for delete account button
    
    * feat: Create deleteUser method to delete users and their data
    
    * bug: Prevent user from going back to previous screens after deletion
    
    * refactor: Remove unused field
    
    ---------
    
    Co-authored-by: KhiemNguyen15 <khiemsa15@gmail.com>

--- app/src/main/java/com/github/skyfall/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/github/skyfall/ui/settings/SettingsFragment.java
@@ -1,16 +1,19 @@
 package com.github.skyfall.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 
 import com.github.skyfall.R;
 import com.github.skyfall.data.model.FirebaseManager;
+import com.github.skyfall.ui.login.FirebaseAuthActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     private FirebaseManager firebaseManager;
@@ -33,8 +36,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         // SwitchPreferenceCompat notificationSwitch = findPreference("enableNotifs");
         // Preference clearHistoryButton = findPreference("clearHistory");
         EditTextPreference usernameButton = findPreference("changeUsername");
-        Preference passwordButton = findPreference("changePassword");
         Preference pfpButton = findPreference("changePfp");
+        Preference deleteButton = findPreference("deleteAccount");
 
         /*if(notificationSwitch == null) {
             Toast.makeText(getActivity(), "Notification Switch is null", Toast.LENGTH_LONG).show();
@@ -48,12 +51,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             Toast.makeText(getActivity(), "Username Button is null", Toast.LENGTH_LONG).show();
             return;
         }
-        if (passwordButton == null) {
-            Toast.makeText(getActivity(), "Password Button is null", Toast.LENGTH_LONG).show();
-            return;
-        }
         if (pfpButton == null) {
             Toast.makeText(getActivity(), "Profile Picture Button is null", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (deleteButton == null) {
+            Toast.makeText(getActivity(), "Password Button is null", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -73,5 +76,43 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             mGetContent.launch("image/*");
             return false;
         });
+
+        deleteButton.setOnPreferenceClickListener(preference -> {
+            AlertDialog.Builder builder = getBuilder();
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            return false;
+        });
+    }
+
+    private AlertDialog.Builder getBuilder() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setCancelable(true)
+                .setTitle("Confirm account deletion")
+                .setMessage("Are you sure you want to delete your account?");
+
+        builder.setPositiveButton("Confirm",
+                (dialog, which) -> {
+                    firebaseManager.deleteUser()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getActivity(), "Your account has been deleted", Toast.LENGTH_LONG).show();
+
+                                    Intent intent = new Intent(getActivity(), FirebaseAuthActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                    requireActivity().finish();
+                                } else {
+                                    Toast.makeText(getActivity(), "Unable to delete account", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                });
+
+        builder.setNegativeButton(android.R.string.cancel, (dialog, which) -> {
+        });
+
+        return builder;
     }
 }
